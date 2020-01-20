f413263cd7e29dca7e131a12a0ed58ddd981e403
kryssyco <kjshepard70@hotmail.com>
2020-01-19 22:32:28 -0700

    Updated df_utils.py with actual functions and uploaded to test pypi

--- lambdata_krissy/__init__.py
+++ lambdata_krissy/__init__.py
@@ -5,5 +5,7 @@ lambdata - a collection of Data Science helper functions
 import pandas as pd 
 import numpy as np
 
+# Sample Code
+
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
--- lambdata_krissy/df_utils.py
+++ lambdata_krissy/df_utils.py
@@ -2,6 +2,23 @@
 Utility functions for working with dataframes
 """
 
-import pandas
+import numpy as np
+import pandas as pd
 
-TEST_DF = pandas.DataFrame([1,2,3])
+# Check a dataframe for nulls, print/report them in a nice "pretty" format
+
+
+def get_info(dataframe):
+    print(dataframe.shape)
+    print (dataframe.isna().sum())
+
+# add a row onto a dataframe that is filled with randomized numbers set by user
+
+
+def randrow(dataframe, min=0, max=100):
+    i = 0
+    while 'randrow_{}'.format(i) in dataframe.columns:
+        i += 1
+    dataframe['randrow_{}'.format(i)] = pd.Series(np.random.randint(min, max,
+                                                  size=len(dataframe)))
+    return dataframe
